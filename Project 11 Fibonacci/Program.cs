01314b968dea749e30767b401ce98afdfaf3118f
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 04:16:09 +0300

    Initial commit

---
+++ Project 11 Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_11_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            //Console.WriteLine("Hangi Değere Kadar Fibonacci Dizisi Oluşturulsun:");
+            //N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İlk Kaç Fibonacci Sayısı Oluşturulsun:");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            int x = 1;
+            int y = 1;
+
+            int z=x+y;
+
+            Console.Write(x+" "+y+" "+z);
+
+            for (int i=0; i <= 6; i++ )
+            {
+                x = y;
+                y = z;
+                z = x + y;
+                Console.Write(" "+z);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_44_Fibonacci
+ 
